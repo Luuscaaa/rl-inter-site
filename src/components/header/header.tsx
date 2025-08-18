70beae179be06922579deea235777a9d688ad370
Lucas Santana <Lucassantana_m@hotmail.com>
2025-08-18 20:53:08 -0300

    init: estrutura inicial do projeto

---
+++ src/components/header/header.tsx
@@ -0,0 +1,11 @@
+import styles from './header.module.css';
+
+function Header() {
+  return (
+    <div className={styles.container}>
+      <h1>TESTE</h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/header/header";
+import styles from "./home.module.css";
+
+function Home() {
+  return (
+    <div className={styles.container}>
+      <Header />
+    </div>
+  );
+}
+
+export default Home;
